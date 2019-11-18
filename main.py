64fc0ac4ede234437702eb9e522cbfd8fe749c3b
j2755 <46614024+j2755@users.noreply.github.com>
2019-11-18 14:38:07 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,41 +2,28 @@
 Code of How to create Stopwatch 
 '''
      
-# Program to Show how to create a switch  
-# import kivy module     
+   
 import kivy   
-         
-# base Class of your App inherits from the App class.     
-# app:always refers to the instance of your application    
+  
 from kivy.app import App  
        
-# this restrict the kivy version i.e   
-# below this kivy version you cannot   
-# use the app or software   
+  
 kivy.require('1.9.0') 
    
-# The Builder is responsible for creating 
-# a Parser for parsing a kv file 
+
 from kivy.lang import Builder 
-  
-# The Properties classes are used 
-# when you create an EventDispatcher. 
+
 from kivy.properties import NumericProperty 
 from kivy.properties import StringProperty
   
-# BoxLayout arranges children in a vertical or horizontal box.  
-# or help to put the children at the desired location.  
+  
 from kivy.uix.boxlayout import BoxLayout  
   
 # he Clock object allows you to 
-# schedule a function call in the future 
+
 from kivy.clock import Clock 
 import datetime as dt
-  
-# Create the .kv file and load it by using Builder 
 
-   
-# Create the Layout class 
 class MainWidget(BoxLayout): 
     time_val=dt.datetime.now()
     
